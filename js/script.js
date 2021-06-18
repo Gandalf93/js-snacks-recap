f24ddb778d645c6b5507da1e6f04ea26643ce94f
Giulia <giuliatullia93@gmail.com>
2021-06-18 15:27:34 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+// Es: 
+// a[2,11,6,77,3,4,9];
+// a = 1;
+// b = 3;
+// mi restituisce a [11,6,77]
